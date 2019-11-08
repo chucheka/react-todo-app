d5d802346ef4fca3f6667b50a753f820ffcd9ef4
ucheka22 <ryanucheka@gmail.com>
2019-11-08 03:19:23 +0100

    Updates

---
+++ precache-manifest.be67f72e758956e3cca52d983e51c01d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "800010dfa75d0c732167fdf2deba7afa",
+    "url": "/ucheka22/react-todo-app/index.html"
+  },
+  {
+    "revision": "ea1e73919247402e6df5",
+    "url": "/ucheka22/react-todo-app/static/css/main.94465a6c.chunk.css"
+  },
+  {
+    "revision": "f29410b88d34670241db",
+    "url": "/ucheka22/react-todo-app/static/js/2.98fbe5dd.chunk.js"
+  },
+  {
+    "revision": "ea1e73919247402e6df5",
+    "url": "/ucheka22/react-todo-app/static/js/main.8ec64c37.chunk.js"
+  },
+  {
+    "revision": "425186ccc902442df7a9",
+    "url": "/ucheka22/react-todo-app/static/js/runtime~main.ac7de61f.js"
+  }
+]);
